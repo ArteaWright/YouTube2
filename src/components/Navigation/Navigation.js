b5ff4a6cd964950f8cf44ec273d4c5a5d1a6de3c
Fredrick Evans <Fredrickevans21@gmail.com>
2021-02-22 18:48:47 -0500

    This is Fred's Update

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,27 +1,28 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import VideoList from '../VideoList/VideoList';
 import Videos from '../Videos/Videos'
 import Youtube from '../../API/Youtube';
 import './Navigation.css';
 import SearchBar from '../SearchBar/SearchBar';
+import VoiceRec from "../VoiceRec/VoiceRec"
 
 class Nav extends Component {
 
-render() {
-    return(
-        <div className="outerNav">
-            <nav className="Navigation">
-                {/* Hamburger Menu */}
-                <h4>Hamburger Menu</h4>
-                {/* Logo */}
-                <h4 className="Logo">Logo</h4>
-            {/* other Icons */}
-            <h4>Other Icons</h4>
-            </nav>
-        </div>
-        
-    );
-}
+    render() {
+        return (
+            <div className="outerNav">
+                <nav className="Navigation">
+                    {/* Hamburger Menu */}
+                    <h4>Hamburger Menu</h4>
+                    {/* Logo */}
+                    <h4 className="Logo">Logo</h4>
+                    {/* other Icons */}
+                    <h4>Other Icons <VoiceRec onRecord={(msg) => { console.log(msg) }} /></h4>
+                </nav>
+            </div>
+
+        );
+    }
 }
 
 export default Nav;
