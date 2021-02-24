6c5b3ff68df3b0ab6a01b7fabd5b2dcc2cbab19c
Aaron Moua <aaronmoua@MacBook-Pro-3.attlocal.net>
2021-02-24 14:54:34 -0500

    update voice

--- src/App.js
+++ src/App.js
@@ -6,13 +6,7 @@ function App() {
   return (
     <div>
       <Home/>
-      {/* <ContactUs/> */}
-      {/* <h2>Hamburger Menu</h2>
-      <h2>Logo</h2>
-      <h3>Search Bar</h3>
-      <h3>Icon</h3>
-      <h4>Card Box Components</h4>
-      <h4>Contact Link</h4> */}
+     
     </div>
   );
 }
--- src/components/VoiceRec/VoiceRec.js
+++ src/components/VoiceRec/VoiceRec.js
@@ -1,28 +1,25 @@
 import React from "react";
-
 class VoiceRec extends React.Component {
     constructor(props) {
         super(props)
         this.startRecorder = this.startRecorder.bind(this)
     }
     startRecorder() {
-
         // Initialze the speech recognition object
         window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
         // Create A New instance called regognition
         let recognition = new window.SpeechRecognition();
-
         recognition.start();
-
         console.log(this.props)
-
         // captures the input from the users speech
         const onSpeak = (e) => {
             // console.log(e);
             const msg = e.results[0][0].transcript;
             this.props.onRecord(msg)
-            const Voice = document.querySelector('.Search').value;
+            // const Voice = document.querySelector('.Search').value;
+            const Voice = document.querySelector('.Search');
+            Voice.value = msg;
+            Voice.focus();
             console.log(Voice)
         }
         recognition.addEventListener('result', onSpeak);
@@ -34,7 +31,5 @@ class VoiceRec extends React.Component {
             </div>
         )
     }
-
 };
-
 export default VoiceRec;
