0a01e41de18bc7e6cf4d82ff96d390e82c0b8939
Robert Young <robertyoung@MacBook-Pro-3.lan>
2021-03-14 17:50:02 -0400

    addind cors

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var videosRouter = require('./routes/videos');
+const cors = require('cors');
+
 
 
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
