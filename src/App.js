042f03c0dc8c5a7a4b18091777a35d16575504e1
Aaron Moua <aaronmoua@MacBook-Pro-4.lan>
2021-03-15 22:43:24 -0400

    updated side

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Home from './Pages/Home';
 import ContactUs from './Pages/ContactUs/ContactUs';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="background">
       <Home/>
-      
+
     </div>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,45 +1,56 @@
-import React, { Component } from 'react';
-import Nav from '../components/Navigation/Navigation'
-import VideoList from '../components/VideoList/VideoList';
-import Videos from '../components/Videos/Videos';
-import Youtube from '../API/Youtube';
-import SearchBar from '../components/SearchBar/SearchBar';
-import CardBox from '../components/CardBox/CardBox';
-import Sidebar from '../components/Sidebar/Sidebar';
-
+import React, { Component } from "react";
+import Nav from "../components/Navigation/Navigation";
+import VideoList from "../components/VideoList/VideoList";
+import Videos from "../components/Videos/Videos";
+import Youtube from "../API/Youtube";
+import SearchBar from "../components/SearchBar/SearchBar";
+import CardBox from "../components/CardBox/CardBox";
+import Sidebar from "../components/Sidebar/Sidebar";
+import "./Home.css";
 
 class Home extends Component {
-    state = {
-        videos: [],
-        selectedVideo: null,
-        voiceSearch: ""
-    }
+  state = {
+    videos: [],
+    selectedVideo: null,
+    voiceSearch: "",
+  };
 
-    handleSubmit = async (searchWord) => {
-        const respond = await Youtube.get('search', { params: { q: searchWord } });
+  handleSubmit = async (searchWord) => {
+    const respond = await Youtube.get("search", { params: { q: searchWord } });
 
-        console.log(respond.data.items);
-        this.setState({ videos: respond.data.items, selectedVideo: respond.data.items[0] });
-        document.querySelector('.outerWrap').style.display = 'none';
-    }
+    console.log(respond.data.items);
+    this.setState({
+      videos: respond.data.items,
+      selectedVideo: respond.data.items[0],
+    });
+    document.querySelector(".outerWrap").style.display = "none";
+  };
 
-    onSelect = (videos) => {
-        this.setState({ ...this.state, selectedVideo: videos.videos });
-    }
-    render() {
-        const { videos, selectedVideo } = this.state;
-        return (
-            <React.Fragment>
-
-                <Nav onRecord={(msg) => { this.setState({ voiceSearch: msg }) }} />
-                <SearchBar value={this.state.voiceSearch} onFormSubmit={this.handleSubmit} />
-                <Videos videos={selectedVideo} />
-                <VideoList videos={videos} onSelect={this.onSelect} />
-                <Sidebar/>
-                <CardBox />
-            </React.Fragment>
-        );
-    }
+  onSelect = (videos) => {
+    this.setState({ ...this.state, selectedVideo: videos.videos });
+  };
+  render() {
+    const { videos, selectedVideo } = this.state;
+    return (
+      <React.Fragment>
+        <Nav
+          onRecord={(msg) => {
+            this.setState({ voiceSearch: msg });
+          }}
+        />
+        <SearchBar
+          value={this.state.voiceSearch}
+          onFormSubmit={this.handleSubmit}
+        />
+        <Videos videos={selectedVideo} />
+        <VideoList videos={videos} onSelect={this.onSelect} />
+        <div className="app-page">
+          <Sidebar />
+          <CardBox />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Home;
